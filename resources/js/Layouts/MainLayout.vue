9a52a55504100322fdab3a1a98ed52b027b449dc
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-07-16 14:24:37 +0800

    minor adjustment

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,7 +3,7 @@
       <Sidebar v-if="user.club_registers?.length > 0 || user.role === 'admin'" class="ml-[-16rem] lg:ml-0" />
       <div v-if="user.club_registers?.length > 0 || user.role === 'admin'" class="flex flex-col flex-1 flex-grow overflow-y-auto bg-gray-100">
         <TopNav class="lg:hidden" />
-        <div class="px-8 py-8">
+        <div class="md:px-8 px-2 py-8">
           <slot />
         </div>
       </div>
