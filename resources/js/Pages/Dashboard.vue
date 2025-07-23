fb51a4f1f1b855ded4f7d61510256f7ce052f757
Leonardo Empuesto <brainsnap.2012@gmail.com>
2025-07-23 13:25:32 +0800

    minor fix

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -118,7 +118,7 @@ const externalink = computed(() => page.props?.auth?.user.club_registers?.[0]?.e
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M9 12l2 2 4-4" />
                             </svg>
-                            <span class="text-gray-700 dark:text-gray-200 font-medium">Accreditation</span>
+                            <span class="text-gray-700 dark:text-gray-200 font-medium">Accreditation and Reaccreditation</span>
                             </div>
                             <svg class="w-5 h-5 text-indigo-400 dark:text-indigo-300" fill="none" stroke="currentColor"
                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
