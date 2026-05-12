527b61cb7907da2fc9f34040279e6ae528443d66
Leonardo Empuesto <brainsnap.2012@gmail.com>
2026-05-12 09:30:53 +0800

    Update ClubOfficerStanding.vue

--- resources/js/Components/club/forms/ClubOfficerStanding.vue
+++ resources/js/Components/club/forms/ClubOfficerStanding.vue
@@ -90,7 +90,7 @@
                 {{ officer.position }}
               </td>
               <td class="border border-black px-2 py-1 text-center">
-                Outstanding
+                Good Standing
               </td>
             </tr>
           </tbody>
